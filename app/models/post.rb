c465aa8f13629bb294c75afa1591d8f7eff7ee81
Kola Kolade <kolakolade@gmail.com>
2023-07-13 20:01:26 +0100

    Update post model to add method to retrieve last 5 comments and method to update post count

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :delete_all
   has_many :likes, dependent: :delete_all
+
+  def update_post_counter
+    author.update_attribute('post_counter', author.posts.count)
+  end
+
+  def last_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
