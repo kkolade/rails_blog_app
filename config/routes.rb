5cc54564d16fea5cbf58d87697ea71ebce009100
Kola Kolade <kolakolade@gmail.com>
2023-07-17 18:00:59 +0100

    Set up routes for the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "Users#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only[:index, :show] do
+    resources :posts, only[:index, :show, :new, :create, :destroy] 
+  end
 end
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
