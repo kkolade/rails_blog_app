e994089bab50b8f131e65b8a008643fa02d8d129
Kola Kolade <kolakolade@gmail.com>
2023-07-17 18:16:22 +0100

    Set up views for the Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root "Users#index"
+  root "users#index"
 
-  resources :users, only[:index, :show] do
-    resources :posts, only[:index, :show, :new, :create, :destroy] 
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] 
   end
 end
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
