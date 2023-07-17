c4ba63b6c8e54c397cc6903ba1a65acc7c0404b6
Kola Kolade <kolakolade@gmail.com>
2023-07-17 18:27:05 +0100

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] 
+    resources :posts, only: [:index, :show] 
   end
 end
+
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
