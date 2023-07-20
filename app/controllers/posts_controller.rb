b43ba6b4b3acb99373fd8c4733955708d3ca7f25
Kola Kolade <kolakolade@gmail.com>
2023-07-20 08:02:25 +0100

    Fix all issues raised by the reviewer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,7 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @posts = @user.posts
+    @post_number = @user.posts.order(created_at: :asc).pluck(:id).index(@post.id) + 1
   end
 end
