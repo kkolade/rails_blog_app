003d9020d3298ab46ae153390caa54e93f4138b6
Kola Kolade <kolakolade@gmail.com>
2023-07-21 22:51:22 +0100

    Add "new" and "create" def to the Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
