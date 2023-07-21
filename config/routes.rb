7b1ea35a673a6e81f43eb15972e65c3fc2c269a9
Kola Kolade <kolakolade@gmail.com>
2023-07-21 22:47:35 +0100

    Add scaffold for comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/create'
   root "users#index"
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
+    end
   end
 end
-
-# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
