e1397d7f5a7fffaf98230c30a3c4520c48aed503
Kola Kolade <kolakolade@gmail.com>
2023-07-22 01:20:06 +0100

    Add code to likes#create

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,14 @@
 class LikesController < ApplicationController
   def create
+    def create
+      @like = Like.new
+      @like.author = current_user
+      @like.post = Post.find(params[:post_id])
+      if @like.save
+        redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
   end
 end
