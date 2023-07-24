aecc5125714bff9e745e49d2b3651d103a730922
Kola Kolade <kolakolade@gmail.com>
2023-07-24 18:13:49 +0100

    Run migration

--- db/migrate/20230715221301_remove_duplicate_add_comment_counter_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDuplicateAddCommentCounterToPosts < ActiveRecord::Migration[7.0]
-  def change
-    # Remove the duplicate column migration
-    remove_column :posts, :comment_counter
-  end
-end
--- db/migrate/20230722105447_add_likes_count_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddLikesCountToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :likes_count, :integer, default: 0
-  end
-end
