c6926ac1361b6efbf909d6e242c00edc788061e8
Kola Kolade <kolakolade@gmail.com>
2023-07-24 19:00:35 +0100

    Refactor typo in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def update_comment_counter
-    post.update(comment_counter: post.comments.count)
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
   end
 
   validates :text, presence: true
-  after_save :update_comment_counter
+  after_save :update_comments_counter
 end
