452ebaa5d468a601e453ec03fb1c4c936543de12
Kola Kolade <kolakolade@gmail.com>
2023-08-03 18:46:34 +0100

    I create Registration and Sessions Controllers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Set the default URL for Devise -me
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
