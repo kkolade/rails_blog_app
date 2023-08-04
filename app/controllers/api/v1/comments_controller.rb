0f6ab85a2e8287772ef8ea5ce52714020dba5e96
Kola Kolade <kolakolade@gmail.com>
2023-08-04 12:39:44 +0100

    Update the new controller actions to handle the API requests and responses

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::CommentsController < ApplicationController
+  def index
+    post = Post.find(params[:post_id])
+    comments = post.comments
+    render json: comments
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    user = User.find_by(email: params[:email]) # Assuming you pass user's email in the request
+    comment = post.comments.new(user: user, content: params[:content])
+    
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: { error: comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
