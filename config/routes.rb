bd79e17df8d352e372b7fa597c8b9f3e31f2c28c
Kola Kolade <kolakolade@gmail.com>
2023-08-04 12:32:15 +0100

    Add routes for API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  
+
+  # HTML routes for authenticated users
   authenticated :user do
     root to: 'users#index', as: :authenticated_root
   end
@@ -14,6 +15,17 @@ Rails.application.routes.draw do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
       resources :likes, only: [:create]
+    end
+  end
+
+  # API routes
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show, :create] do
+          resources :comments, only: [:index, :create]
+        end
+      end
     end
   end
 end
